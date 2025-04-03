7ce155bd51dc26b28afc52f5529812209dc9790a
Eliza Kotecka <elizaganca@gmail.com>
2025-04-03 14:09:04 +0200

    created authentication app

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'main',
     'news',
     'forum',
+    'authentication',
 ]
 
 MIDDLEWARE = [ # now default, used for verification/modification processing data from requests
