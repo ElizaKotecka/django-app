6d822ea5896b602272ac81ece38e4bc7d3baf952
Eliza Kotecka <elizaganca@gmail.com>
2025-04-25 12:39:50 +0200

    add post only for authenticated users

--- webapp/main/templatetags/sidebar_links.py
+++ webapp/main/templatetags/sidebar_links.py
@@ -1,10 +1,36 @@
 from django import template
 
-register = template.Library();
+register = template.Library()
 
 @register.simple_tag
 def get_links():
-    return [{
+    links =  [{
+        'name': 'Home',
+        'href': '/',
+        'icon': 'fa-house',
+    }, {
+        'name': 'Contact',
+        'href': '/contact',
+        'icon': 'fa-paper-plane',
+    }, {
+        'name': 'About',
+        'href': '/about',
+        'icon': 'fa-address-card',
+    },{
+        'name': 'Marketplace',
+        'href': '/marketplace/',
+        'icon': 'fa-newspaper',
+    },{
+        'name': 'Forum',
+        'href': '/forum',
+        'icon': 'fa-comment', #look for your icon here https://fontawesome.com/search?ic=free
+    }]
+
+    return links
+
+@register.simple_tag
+def get_links_authenticated():
+    links =  [{
         'name': 'Home',
         'href': '/',
         'icon': 'fa-house',
@@ -29,4 +55,5 @@ def get_links():
         'href': '/forum',
         'icon': 'fa-comment', #look for your icon here https://fontawesome.com/search?ic=free
     }]
-    
+         
+    return links
