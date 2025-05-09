1356338920496b2254d75641884be0a20774629f
Eliza Kotecka <elizaganca@gmail.com>
2025-05-09 14:50:31 +0200

    changed type of filed in published_at

--- webapp/news/models.py
+++ webapp/news/models.py
@@ -6,7 +6,7 @@ class News(models.Model):
     title = models.CharField('Post title', max_length=100, null=False, blank=False)
     excerpt = models.CharField('Post excerpt', max_length=250, null=False, blank=False)
     body = models.TextField('Post body', null=False, blank=True)
-    published_at = models.DateField('Post published at', null=False, blank=False)
+    published_at = models.DateTimeField('Post published at', null=False, blank=False)
 
     def get_absolute_url(self):
         return f'/news/{self.id}'
