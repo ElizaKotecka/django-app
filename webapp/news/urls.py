93a312a1d97d341b7488059b9f98f506f2366d91
Eliza Kotecka <elizaganca@gmail.com>
2025-05-09 13:54:37 +0200

    adding news only from admin panel

--- webapp/news/urls.py
+++ webapp/news/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views # from current folder import the neighbour file views for views methods usage
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.news_home, name ='news_home'),
+    path('<int:pk>', views.NewsDetailView.as_view(), name='news_show'),
 ]
  
